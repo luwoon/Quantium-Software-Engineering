c55989077c4a62c0c1a63f0a8a178009049e5512
luwoon <66871214+luwoon@users.noreply.github.com>
2024-04-29 10:15:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
 df = pd.read_csv('output.csv')
 
+unique_regions = df['region'].unique()
+
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(children='Sales increased after the Pink Morsel price increase on the 15th of January, 2021.', style={'textAlign':'center'}),
-    dcc.Graph(figure=px.line(df, x='date', y='sales'))
+    html.Hr(),
+    dcc.RadioItems(
+        options=[{'label': 'all', 'value': 'all'}] + [{'label': region, 'value': region} for region in unique_regions], 
+        value='All', 
+        id='controls-and-radio-item'
+    ),
+    dcc.Graph(figure={}, id='controls-and-graph')
 ])
+
+@app.callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    [Input(component_id='controls-and-radio-item', component_property='value')]
+)
+def update_graph(region_selected):
+    if region_selected == 'All':
+        filtered_df = df
+    else:
+        filtered_df = df[df['region'] == region_selected]
+    
+    fig = px.line(filtered_df, x='date', y='sales')
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
